efa2382865106ea5a488005b78d6637679a2f774
ivans <ivan.samoyloff@gmail.com>
2016-12-01 18:18:24 +0200

    mission complete ver5.0

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-var result;
-result = prompt('Enter the string');
-function Lucas() {
-    result = result.match(/[a-z]|[а-я]/gi);
-    result.sort();
-    return result;
+var input;
+var matches;
+input = prompt('Enter the string');
+function Lucas(b) {
+    b = b.match(/[a-z]|[а-я]/gi);
+    b.sort();
+    matches = b;
+    return b;
 }
-function Count(){
+function Count(a){
     var obj={};
-    for(var i=0;i<result.length;i++) {
-        var key = result[i];
+    for(var i=0;i<a.length;i++) {
+        var key = a[i];
         if(!obj[key]){
             obj[key]=0;
         }
         obj[key]+=1;
     }
     return obj;
+
 }
-Lucas(result);
-Count(result);
+Lucas(input);
+Count(matches);
